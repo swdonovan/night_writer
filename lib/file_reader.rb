815214fd62406e483e6fd8d90524a89447e9adad
Wil_D <wil.donovan@gmail.com>
2017-05-13 11:30:58 -0600

    initial FileReader

---
+++ lib/file_reader.rb
@@ -0,0 +1,17 @@
+# require_relative 'night_writer'
+class FileReader
+  attr_reader :incoming_text,
+              :file_name
+
+  def initialize
+    @file_name = File.open(ARGV[0], "r")
+    @incoming_text = @file_name.read.chomp
+    # @file_name.close
+  end
+
+end
+
+
+# words = FileReader.new
+#
+# puts words.incoming_text
