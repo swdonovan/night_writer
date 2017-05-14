b204e693de45a76e54230309e899849832c1aa56
JF-Lalonde <lalondej88@gmail.com>
2017-05-14 08:43:34 -0600

    Adding some tests

--- test/file_reader_test.rb
+++ test/file_reader_test.rb
@@ -2,17 +2,35 @@ gem 'minitest', '~>5.0'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/night_writer'
-
+require 'pry'
 class FileReaderTest < Minitest::Test
 
-
-
-  def test_if_file_name_is_correct
-    file_name = ARGV[0]
-
-    assert_equal message.txt, file_name
+  def test_if_ARGV_has_a_value
+    assert_equal "lib/message.txt", ARGV[0]
   end
 
+  def test_if_file_name_is_correct
+    skip
+    name = File.open(ARGV[0], "r")
+
+    assert_equal " #<File:lib/message.txt>", name
+  end
+
+  def test_if_incoming_text_is_captured
+    name = File.open(ARGV[0], "r")
+    incoming_text = name.read
+
+    assert_equal "Hello everyone. Test test\n", incoming_text
+  end
+
+class NightWriterTest < Minitest::Test
+  def test_NightWriter_initializes_FileReader
+    skip
+    words = FileReader.new
+
+    assert_equal words.reader, @reader
+  end
+end
 
 
 
