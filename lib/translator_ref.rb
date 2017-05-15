124701499b21550dcd1b23b321f09ee0dcd8fda3
Wil_D <wil.donovan@gmail.com>
2017-05-15 17:11:38 -0600

    new hopefully

--- lib/translator_ref.rb
+++ lib/translator_ref.rb
@@ -1,7 +1,7 @@
 class Translator
 
   def initialize(input)
-    @translated_input = incoming_text
+    @translated_input = input
     @braille_hash = {"top_line" => {"abehkloruvz" => "0.", "cdfgmnpqxy" => "00", "ijstw"=>".0"},
                      "middle_line" => {"bfilpsv" => "0.", "ghjqrtw" => "00", "denoyz" => ".0", "ackmux" => ".."},
                      "bottom_line" => {"klmnopqrst" => "0.", "uvxyz" => "00", "abcdefghij" => "..", "w" => ".0"}}
@@ -9,15 +9,15 @@ class Translator
     @braille_hash_middle = Array.new
     @braille_hash_bottom = Array.new
     @final_braille_on_rails = Array.new
+  end
 
 
-  def translator(input)
-  @translated_input = input
-  @translated_input.split("")
+  def translator
+  @translated_input_array = @translated_input.split("")
   end
 
   def top_line_translate_to_braille
-    @translated_input.split("").each do |x|
+    @translated_input_array.each do |x|
       if braille_hash["top_line"].keys[0].include? x
         @braille_hash_top.push(braille_hash["top_line"].values[0])
       elsif braille_hash["top_line"].keys[1].include? x
@@ -27,11 +27,11 @@ class Translator
       elsif x == " "
         @braille_hash_top.push("..")
       elsif x != " " && x == x.upcase
-        @upcase_to_top_line
+        @upcase_to_top_line(x)
       end
     end
   end
-  def upcase_to_top_line
+  def upcase_to_top_line(x)
     if braille_hash["top_line"].keys[0].include? x.downcase
       @braille_hash_top.push(".." + braille_hash["top_line"].values[0])
     elsif braille_hash["top_line"].keys[1].include? x.downcase
@@ -41,7 +41,7 @@ class Translator
     end
   end
   def middle_line_translate_to_braille
-    @translated_input.split("").each do |x|
+    @translated_input_array.each do |x|
       if braille_hash["middle_line"].keys[0].include? x
         @braille_hash_middle.push(braille_hash["middle_line"].values[0])
       elsif braille_hash["middle_line"].keys[1].include? x
@@ -53,11 +53,11 @@ class Translator
       elsif x == " "
         @braille_hash_middle.push("..")
       elsif x != " " && x == x.upcase
-        @upcase_to_middle_line
+        @upcase_to_middle_line(x)
       end
     end
   end
-  def upcase_to_middle_line
+  def upcase_to_middle_line(x)
     if braille_hash["middle_line"].keys[0].include? x.downcase
       @braille_hash_middle.push(".." + braille_hash["middle_line"].values[0])
     elsif braille_hash["middle_line"].keys[1].include? x.downcase
@@ -70,7 +70,7 @@ class Translator
   end
 
   def bottom_line_translate_to_braille
-    @translated_input.split("").each do |x|
+    @translated_input_array.each do |x|
       if braille_hash["bottom_line"].keys[0].include? x
         @braille_hash_bottom.push(braille_hash["bottom_line"].values[0])
       elsif braille_hash["bottom_line"].keys[1].include? x
@@ -82,12 +82,12 @@ class Translator
       elsif x == " "
         @braille_hash_bottom.push("..")
       elsif x != " " && x == x.upcase
-        @upcase_to_bottom_line
+        @upcase_to_bottom_line(x)
       end
     end
   end
 
-  def upcase_to_bottom_line
+  def upcase_to_bottom_line(x)
     if braille_hash["bottom_line"].keys[0].include? x.downcase
       @braille_hash_bottom.push(".0" + braille_hash["bottom_line"].values[0])
     elsif braille_hash["bottom_line"].keys[1].include? x.downcase
@@ -98,7 +98,4 @@ class Translator
       @braille_hash_bottom.push(".0" + braille_hash["bottom_line"].values[3])
     end
   end
-
-
-
 end
